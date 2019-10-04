fcb62598e3f6567e27e5c976e73542e29f8d83ed
goraegori <gorae02@gmail.com>
2019-10-05 03:51:14 +0900

    First

---
+++ PInject/PInject/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PApplier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tmrCheckPak = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.chkUsePList = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(196, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "우회";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(371, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\PUBG";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // tmrCheckPak
+            // 
+            this.tmrCheckPak.Interval = 500;
+            this.tmrCheckPak.Tick += new System.EventHandler(this.TmrCheckPUBG_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.chkUsePList);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(389, 147);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(371, 21);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "E:\\res";
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // chkUsePList
+            // 
+            this.chkUsePList.AutoSize = true;
+            this.chkUsePList.Checked = true;
+            this.chkUsePList.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkUsePList.Location = new System.Drawing.Point(6, 126);
+            this.chkUsePList.Name = "chkUsePList";
+            this.chkUsePList.Size = new System.Drawing.Size(119, 16);
+            this.chkUsePList.TabIndex = 3;
+            this.chkUsePList.Text = "Use PakList.json";
+            this.chkUsePList.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 169);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "PInject";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer tmrCheckPak;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkUsePList;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ PInject/PInject/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PApplier
+{
+    public partial class Form1 : Form
+    {
+        string pubgPath,resPath;
+        public Form1()
+        {
+            InitializeComponent();
+            pubgPath = textBox1.Text + @"\TslGame\Content\Paks";
+            resPath = textBox2.Text;
+        }
+        async Task Wait2Sec()
+        {
+            await Task.Delay(2000, new CancellationTokenSource().Token);
+        }
+        private void CreateSL(string dst, string src)
+        {
+            if(File.Exists(dst)) File.Delete(dst);
+            ProcessStartInfo pi = new ProcessStartInfo();
+            pi.WindowStyle = ProcessWindowStyle.Hidden;
+            pi.FileName = "cmd";
+            pi.Arguments = "/c \"mklink \"" + dst + "\" \"" + src + "\"\"";
+            Process.Start(pi);
+        }
+        private void PreInjectPak(string pakName)
+        {
+            String org = pubgPath + @"\" + pakName,
+                mid = Application.CommonAppDataPath + @"\" + pakName;
+            CreateSL(org, mid);
+        }
+        private void InjectPak(string pakName)
+        {
+            String mid = Application.CommonAppDataPath + @"\" + pakName,
+                src = resPath + @"\" + pakName;
+            CreateSL(mid, src);
+        }
+        private void EjectPak(string pakName)
+        {
+            String mid = Application.CommonAppDataPath + @"\" + pakName,
+                src = Application.StartupPath + @"\org." + pakName.Split('.')[1];
+            CreateSL(mid, src);
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            foreach ( FileInfo eachPak in new DirectoryInfo(resPath).GetFiles("*.pak"))
+            {
+                PreInjectPak(eachPak.Name);
+                EjectPak(eachPak.Name);
+            }
+            if (chkUsePList.Checked)
+            {
+                PreInjectPak("pakList.json");
+                EjectPak("pakList.json");
+            }
+            tmrCheckPak.Start();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            pubgPath = textBox1.Text + @"\TslGame\Content\Paks";
+        }
+
+        private async void TmrCheckPUBG_Tick(object sender, EventArgs e)
+        {
+            Text = "Pak 적용 대기중";
+            if (Process.GetProcessesByName("TslGame").Length >= 2)
+            {
+                await Wait2Sec();
+                if (chkUsePList.Checked)
+                    InjectPak("pakList.json");
+                foreach (FileInfo eachPak in new DirectoryInfo(resPath).GetFiles("*.pak"))
+                    InjectPak(eachPak.Name);
+                Text = "적용완료";
+                tmrCheckPak.Stop();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            tmrCheckPak.Stop();
+            foreach (FileInfo eachPak in new DirectoryInfo(resPath).GetFiles("*.pak"))
+                EjectPak(eachPak.Name);
+            if (chkUsePList.Checked)
+                EjectPak("pakList.json");
+            Text = "우회 완료";
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            resPath = textBox2.Text;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (FileInfo eachPak in new DirectoryInfo(resPath).GetFiles("*.pak"))
+                EjectPak(eachPak.Name);
+            EjectPak("pakList.json");
+        }
+    }
+}
