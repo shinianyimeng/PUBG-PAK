61fb41711dad620a380c4b7ca3f8dc6b9a05f401
goraegori <gorae02@gmail.com>
2019-10-10 10:15:25 +0900

    added function which Wait for VHD to attach

--- PInject/PInject/Form1.cs
+++ PInject/PInject/Form1.cs
@@ -34,6 +34,20 @@ namespace PApplier
             }
             return "E:\\";
         }
+        private void WaitForAttach()
+        {
+            while (true)
+            {
+                DriveInfo[] drive = DriveInfo.GetDrives();
+                for (int i = 0; i < drive.Length; i++)
+                {
+                    if (drive[i].VolumeLabel == "PUBG")
+                    {
+                        return;
+                    }
+                }
+            }
+        }
         private void AttachVHD()
         {
             ProcessStartInfo pi = new ProcessStartInfo();
@@ -79,6 +93,8 @@ namespace PApplier
         }
         private void Button1_Click(object sender, EventArgs e)
         {
+            AttachVHD();
+            WaitForAttach();
             foreach (FileInfo eachPak in new DirectoryInfo(resPath).GetFiles("*.pak"))
             {
                 PreInjectPak(eachPak.Name);
@@ -133,15 +149,17 @@ namespace PApplier
             textBox3.Text = (int.Parse(textBox3.Text) + 100).ToString();
         }
 
-        private async void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
             AttachVHD();
-            await WaitnSec(1000);
+            WaitForAttach();
             textBox2.Text = GetVHDVolumeLabel() + "res";
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
+            AttachVHD();
+            WaitForAttach();
             foreach (FileInfo eachPak in new DirectoryInfo(resPath).GetFiles("*.pak"))
                 EjectPak(eachPak.Name);
             EjectPak("pakList.json");
